9a69e38a8c02c60461f9677eb76d121ba27215b6
Fahmi <hji930739@gmail.com>
2025-03-26 00:13:37 +0700

    Add feature logout

--- Data.EF/obj/Debug/net8.0/Data.EF.AssemblyInfo.cs
+++ Data.EF/obj/Debug/net8.0/Data.EF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data.EF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce1386125c71f1a897b55d2dc6a9e31e63f9f7d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data.EF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data.EF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ecommerce-api/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Data.Entity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ecommerce_api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class AccountController : ControllerBase
+    {
+       private readonly SignInManager<AppUser> _signInManager;
+       private readonly ILogger<AccountController> _logger;
+       public AccountController(SignInManager<AppUser> signInManager, ILogger<AccountController> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User Log out");
+            return Ok(new { message = "Logged Out Success" });
+        }
+    }
+}
